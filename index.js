1c8d809f90d66c6ff2bcc9a6da95fb074dcff4a7
Hamlet593 <hamomuradyan93@gmail.com>
2021-06-05 17:45:52 +0400

    trying to add delete function at home

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import render from './render.js'
-
 const items = [];
 
 let currentValue = '';
@@ -16,3 +14,19 @@ todo_buton.addEventListener('click', () => {
   items.push({title: currentValue, id: items.length});
   render(items, todo_ulEl)
 })
+
+let deleteButton = document.createElement('Button');
+deleteButton.innerHTML = ' X';
+
+deleteButton.addEventListener('click', () => {
+  todo_ulEl.li.innerHTML = '';
+})
+
+function render(items, todo_ulEl){
+  todo_ulEl.innerHTML = '';
+  items.forEach(item => {
+    todo_ulEl.appendChild(document.createElement('li')).innerHTML = item.title + deleteButton.innerHTML;
+
+  })
+}
+
